6e709eac326fc201f4ee5ad3d2942fb68e345e11
patrickreinan <patrickreinan@gmail.com>
2022-10-02 22:55:56 -0300

    models

---
+++ vehicles-api/VehiclePostRequest.cs
@@ -0,0 +1,5 @@
+﻿internal class VehiclePostRequest
+{
+    public string Name { get; set; }
+    public string Brand { get; set; }
+}
---
+++ vehicles-api/VehicleSearchRequest.cs
@@ -0,0 +1,22 @@
+﻿internal class VehicleSearchRequest
+{
+    public string Name { get; set; }
+    public string Brand { get; set; }
+    public int? Page { get; set; }
+    public int? PageSize { get; set; }
+
+    public static ValueTask<VehicleSearchRequest?> BindAsync(HttpContext context)
+        => ValueTask.FromResult<VehicleSearchRequest?>(new VehicleSearchRequest()
+        {
+
+
+            Brand = context.Request.Query["brand"],
+            Name = context.Request.Query["name"],
+
+            Page = int.TryParse(context.Request.Query["page"], out var page) ? page : null,
+            PageSize = int.TryParse(context.Request.Query["pageSize"], out var pageSize) ? pageSize : null
+
+        });
+
+
+}
