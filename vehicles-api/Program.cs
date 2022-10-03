c3f07e257aa16ab9341225e31298e59e2df64f5d
patrickreinan <patrickreinan@gmail.com>
2022-10-02 22:57:02 -0300

    endpoints

--- vehicles-api/Program.cs
+++ vehicles-api/Program.cs
@@ -3,38 +3,119 @@ using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services
     .AddSingleton<IVehicleRepository, InMemroyVehicleRepository>()
     .AddScoped<VehicleService>();
 
-    
-
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
 
-app.MapGet("/", () =>
+app.MapGet("/", () => Results.Ok(new { api = "Vehicles API" }));
+
+
+app.MapPost("/v1/vehicles", (VehiclePostRequest request, VehicleService service) =>
 {
-    return Results.Ok(new { api = "Vehicles API" });
+
+    try
+    {
+        var vehicle = new Vehicle(request.Name, request.Brand);
+        var result = service.Insert(vehicle);
+        var id = result.Result;
+        return Results.Created($"/v1/vehicles/{id}", vehicle);
+    }
+    catch (ArgumentNullException anex)
+    {
+        return Results.BadRequest(anex.Message);
+    }
+
+
+});
+
+app.MapGet("/v1/vehicles/{id}", (string id, VehicleService service) =>
+{
+    var result = service.GetById(id);
+
+    if (result.HasErrors())
+        return Results.BadRequest(result.Errors);
+    else
+
+
+        return result.Result == null
+        ? Results.NotFound()
+        : Results.Ok(result.Result);
+
 
 });
 
 
 
+
+app.MapGet("/v1/vehicles/", (VehicleSearchRequest request, VehicleService service) =>
+{
+    var errorMessages = new List<string>();
+
+    if (!request.Page.HasValue) errorMessages.Add("page cannot be null");
+
+    if (!request.PageSize.HasValue) errorMessages.Add("pageSize cannot be null");
+
+    if (errorMessages.Count > 0)
+        return Results.BadRequest(new { errors = errorMessages.ToArray() });
+
+
+   var result = service.Find(f =>
+     (f.Name == request.Name || string.IsNullOrEmpty(request.Name))
+     &&
+     (f.Brand == request.Brand || string.IsNullOrEmpty(request.Brand))
+     ,
+     request.Page.Value,
+     request.PageSize.Value);
+
+
+    return Results.Ok(result.Result);
+
+});
+
+app.MapPut("/v1/vehicles/{id}", (string id, VehiclePutRequest request, VehicleService service) =>
+{
+
+    try
+    {
+        var vehicle = new Vehicle(id, request.Name, request.Brand);
+        var result = service.Update(vehicle);
+        return result.Result
+        ? Results.NoContent()
+        : Results.NotFound();
+
+    }
+    catch (ArgumentNullException anex)
+    {
+        return Results.BadRequest(anex.Message);
+    }
+
+
+});
+
+app.MapDelete("/v1/vehicles/{id}", (string id, VehicleService service) =>
+{
+
+    try
+    {
+        var result = service.Remove(id);
+        return result.Result
+        ? Results.NoContent()
+        : Results.NotFound();
+
+    }
+    catch (ArgumentNullException anex)
+    {
+        return Results.BadRequest(anex.Message);
+    }
+
+
+});
 app.Run();
 
 
